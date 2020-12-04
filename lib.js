4cf46098344ebaa29af07845e22528a7883450af
Outvi V <git@outv.im>
2020-12-04 20:32:39 +0800

    chore: move bayes.js out

--- lib.js
+++ lib.js
@@ -1,41 +1,6 @@
 const { normalizeSync: normalize_diac } = require('normalize-diacritics')
 
-const BAD_WORDS = {
-  0.8: ['炸群', '@tingpo', '__asm__test__key__'],
-  0.5: [
-    '电报',
-    '土豆',
-    '非小号',
-    '微信：',
-    '在线',
-    '咨询',
-    '增粉',
-    'mytoken',
-    '专卖',
-    '莆田',
-    '推广',
-    '热搜',
-    '承接',
-    '客服',
-    'group',
-    'private',
-    'telegram',
-    'member',
-  ],
-  0.3: [
-    '出售',
-    '联系',
-    '私聊',
-    '加好友',
-    '头像',
-    '推特',
-    '脸书',
-    '油管',
-    '业务',
-    '飞机',
-    'telegram',
-  ],
-}
+const BAD_WORDS = require('./data/bayes')
 
 const NIL_REGEX = [/[-═┅┅┅=]/g]
 
